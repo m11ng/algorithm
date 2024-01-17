12e5aae55daa037578cfc7afc9c848bf2be223e1
bloo_Ming <ming7606@naver.com>
2024-01-17 21:04:23 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 9.40 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -2,6 +2,7 @@ class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = ""; // 새로 담을 그릇
     
+        // 다른 사람 풀이 봄.. 보고 배우자
         // subString 이용
         String str1 = my_string.substring(0,s);
         String str2 = my_string.substring(s + overwrite_string.length());
